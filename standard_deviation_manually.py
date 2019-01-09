11eebf77e64667a5b7847ded86dcef9015934014
Alex Svoboda <46383749+Boda805@users.noreply.github.com>
2019-01-09 13:00:28 -0600

    Add files via upload

---
+++ standard_deviation_manually.py
@@ -0,0 +1,28 @@
+from numpy import array
+from numpy import std
+
+#example data in a list
+data = [86, 75, 76, 91, 65, 67, 80, 78, 86, 88]
+
+#number of data points in list
+n = 10
+
+#calculating mean
+mean = sum(data) / n
+
+#getting relevant data into arrays
+data_array = array(data)
+mean_list_array = array(mean)
+
+#steps to calculating variance/standard deviation
+step1 = data_array - mean_list_array
+step2 = step1 ** 2
+step3 = sum(step2)
+
+#var=variance, sd=standard deviation
+var = step3 / n
+sd = var ** .5
+
+#sd calculated compared to std method
+print(sd)
+print(std(data))
